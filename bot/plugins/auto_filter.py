56ff14f430c60b4b163036a27f884bebc801662f
gokulmoviemedia <84762727+gokulmoviemedia@users.noreply.github.com>
2021-07-11 23:18:10 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>⭕𝗖𝗖 𝗠𝗢𝗩𝗜𝗘𝗦⭕<a href=https://t.me/CCM_Movies >🔰𝘑𝘰𝘪𝘯 𝘊𝘩𝘢𝘯𝘯𝘦𝘭🔰</a>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
